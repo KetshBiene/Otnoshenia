ef2921d14bf488b0acd2e10489fcebb43de15273
IO <IO@IOPC>
2023-06-14 19:03:30 +0300

    Добавьте файлы проекта.

---
+++ Otnoshenia/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace Otnoshenia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.A = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.B = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.C = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.D = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.domainUpDown1 = new System.Windows.Forms.DomainUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(351, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Пуск";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Yellow;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.A,
+            this.B,
+            this.C,
+            this.D});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 63);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(203, 119);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // A
+            // 
+            this.A.HeaderText = "A";
+            this.A.Name = "A";
+            this.A.Width = 40;
+            // 
+            // B
+            // 
+            this.B.HeaderText = "B";
+            this.B.Name = "B";
+            this.B.Width = 40;
+            // 
+            // C
+            // 
+            this.C.HeaderText = "C";
+            this.C.Name = "C";
+            this.C.Width = 40;
+            // 
+            // D
+            // 
+            this.D.HeaderText = "D";
+            this.D.Name = "D";
+            this.D.Width = 40;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.Yellow;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4});
+            this.dataGridView2.Location = new System.Drawing.Point(16, 230);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(203, 119);
+            this.dataGridView2.TabIndex = 2;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "A";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "B";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "C";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "D";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 40;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.BackgroundColor = System.Drawing.Color.Yellow;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8});
+            this.dataGridView3.Location = new System.Drawing.Point(304, 175);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.ReadOnly = true;
+            this.dataGridView3.Size = new System.Drawing.Size(204, 119);
+            this.dataGridView3.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "A";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.ReadOnly = true;
+            this.dataGridViewTextBoxColumn5.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "B";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.ReadOnly = true;
+            this.dataGridViewTextBoxColumn6.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.HeaderText = "C";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.ReadOnly = true;
+            this.dataGridViewTextBoxColumn7.Width = 40;
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.HeaderText = "D";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            this.dataGridViewTextBoxColumn8.ReadOnly = true;
+            this.dataGridViewTextBoxColumn8.Width = 40;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.Color.Yellow;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A∨B",
+            "A∧B",
+            "Не А",
+            "Не B",
+            "A◦B"});
+            this.comboBox1.Location = new System.Drawing.Point(425, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Отношение мноежества А";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(12, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(209, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Отношение мноежества В";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(300, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 40);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Отношение мноежества С \r\n(результат)";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(255, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(164, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Выберете операцию";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // domainUpDown1
+            // 
+            this.domainUpDown1.BackColor = System.Drawing.Color.Yellow;
+            this.domainUpDown1.Items.Add("2х2");
+            this.domainUpDown1.Items.Add("3х3");
+            this.domainUpDown1.Items.Add("4х4");
+            this.domainUpDown1.Items.Add("5х5");
+            this.domainUpDown1.Items.Add("6х6");
+            this.domainUpDown1.Location = new System.Drawing.Point(12, 12);
+            this.domainUpDown1.Name = "domainUpDown1";
+            this.domainUpDown1.ReadOnly = true;
+            this.domainUpDown1.Size = new System.Drawing.Size(135, 20);
+            this.domainUpDown1.TabIndex = 10;
+            this.domainUpDown1.Text = "Размерность матриц";
+            this.domainUpDown1.SelectedItemChanged += new System.EventHandler(this.domainUpDown1_SelectedItemChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(543, 423);
+            this.Controls.Add(this.domainUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Отношения";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn A;
+        private System.Windows.Forms.DataGridViewTextBoxColumn B;
+        private System.Windows.Forms.DataGridViewTextBoxColumn C;
+        private System.Windows.Forms.DataGridViewTextBoxColumn D;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DomainUpDown domainUpDown1;
+    }
+}
+
---
+++ Otnoshenia/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Otnoshenia
+{
+    public partial class Form1 : Form
+    {
+        static double[,] matrixA = new double[,]
+        {
+            {0.3, 0.6, 0.4, 0.9, 0.3, 0.1 },
+            {0.1, 0.7, 0.0, 0.5, 0.1, 0.3 },
+            {1.0, 0.2, 1.0, 0.8, 0.4, 0.5 },
+            {0.5, 0.4, 0.7, 0.6, 1.0, 0.0 },
+            {0.3, 0.6, 0.4, 0.9, 0.3, 0.1 },
+            {1.0, 0.2, 1.0, 0.8, 0.4, 0.5 },
+        };
+
+        static double[,] matrixB = new double[,]
+        {
+            {0.7, 0.3, 0.2, 0.0, 0.3, 0.3 },
+            {0.8, 0.6, 0.4, 0.9, 0.1, 1.0 },
+            {0.6, 0.2, 0.1, 0.1, 0.5, 0.3 },
+            {0.0, 0.3, 0.6, 0.5, 0.2, 0.7 },
+            {0.7, 0.3, 0.2, 0.0, 0.3, 0.3 },
+            {0.6, 0.2, 0.1, 0.1, 0.5, 0.3 },
+        };
+
+        static int N = 4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int operation = comboBox1.SelectedIndex;
+            if(operation == -1) { label4.ForeColor = Color.Red; MessageBox.Show("Выберите операцию", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+
+            try 
+            {
+                double[,] A = new double[N, N];
+                double[,] B = new double[N, N];
+
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        if (Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value) < 0 || Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value) > 1)
+                        { MessageBox.Show($"В ячейке {i}х{j} таблицы А стоит некорректное значение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+                        A[i, j] = Convert.ToDouble(dataGridView1.Rows[i].Cells[j].Value);
+                        if (Convert.ToDouble(dataGridView2.Rows[i].Cells[j].Value) < 0 || Convert.ToDouble(dataGridView2.Rows[i].Cells[j].Value) > 1)
+                        { MessageBox.Show($"В ячейке {i}х{j} таблицы В стоит некорректное значение", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+                        B[i, j] = Convert.ToDouble(dataGridView2.Rows[i].Cells[j].Value);
+                    }
+                }
+                switch (operation)
+                {
+                    case 0:
+                        double[,] C = Union(A, B);
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < N; j++)
+                            {
+                                dataGridView3.Rows[i].Cells[j].Value = C[i, j].ToString();
+                            }
+                        }
+                        break;
+
+                    case 1:
+                        C = Intersec(A, B);
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < N; j++)
+                            {
+                                dataGridView3.Rows[i].Cells[j].Value = C[i, j].ToString();
+                            }
+                        }
+                        break;
+
+                    case 2:
+                        C = Extension(A);
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < N; j++)
+                            {
+                                dataGridView3.Rows[i].Cells[j].Value = C[i, j].ToString();
+                            }
+                        }
+                        break;
+
+                    case 3:
+                        C = Extension(B);
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < N; j++)
+                            {
+                                dataGridView3.Rows[i].Cells[j].Value = C[i, j].ToString();
+                            }
+                        }
+                        break;
+
+                    case 4:
+                        C = Composition(A, B);
+                        for (int i = 0; i < N; i++)
+                        {
+                            for (int j = 0; j < N; j++)
+                            {
+                                dataGridView3.Rows[i].Cells[j].Value = C[i, j].ToString();
+                            }
+                        }
+                        break;
+                }
+            }
+           catch(FormatException) { MessageBox.Show("Неверный тип введённых данных", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); return; }
+        }
+
+        static double[,] Union(double[,] A, double[,] B)
+        {
+            int N = A.GetLength(0);
+            double[,] C = new double[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (A[i, j] >= B[i, j])
+                        C[i, j] = A[i, j];
+                    else
+                        C[i, j] = B[i, j];
+                }
+            }
+            return C;
+        }
+
+        static double[,] Intersec(double[,] A, double[,] B)
+        {
+            int N = A.GetLength(0);
+            double[,] C = new double[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    if (A[i, j] <= B[i, j])
+                        C[i, j] = A[i, j];
+                    else
+                        C[i, j] = B[i, j];
+                }
+            }
+            return C;
+        }
+
+        static double[,] Extension(double[,] A)
+        {
+            int N = A.GetLength(0);
+            double[,] C = new double[N, N];
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    C[i, j] = 1 - A[i, j];
+                }
+            }
+            return C;
+        }
+
+        static double[,] Composition(double[,] A, double[,] B)
+        {
+            double[] min = new double[N];
+            double[,] C = new double[N, N];
+
+            for (int i = 0; i < N; i++)
+            {
+                
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++) 
+                    {
+                        if (A[i, k] < B[k, j])
+                            min[k] = A[i, k];
+                        else
+                            min[k] = B[k, j];
+
+                        if (min[k] > C[i, j])
+                            C[i, j] = min[k];
+                    }
+                }
+            }
+            return C;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.RowCount = N;
+            dataGridView1.ColumnCount = N;
+            dataGridView2.RowCount = N;
+            dataGridView2.ColumnCount = N;
+            dataGridView3.RowCount = N;
+            dataGridView3.ColumnCount = N;
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = matrixA[i, j].ToString();
+                    dataGridView2.Rows[i].Cells[j].Value = matrixB[i, j].ToString();
+                }
+            }
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+            dataGridView2.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView2.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label4.ForeColor = Color.White;
+        }
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+            int dim = domainUpDown1.SelectedIndex;
+            if (dim != -1)
+            {
+                N = dim + 2;
+                dataGridView1.RowCount = N;
+                dataGridView1.ColumnCount = N;
+                dataGridView2.RowCount = N;
+                dataGridView2.ColumnCount = N;
+                dataGridView3.RowCount = N;
+                dataGridView3.ColumnCount = N;
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = matrixA[i, j].ToString();
+                        dataGridView2.Rows[i].Cells[j].Value = matrixB[i, j].ToString();
+                    }
+                }
+            }
+        }
+    }
+}
